f3acb9fbc53f22d85643cde8efa424d008037c7a
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-09 00:39:44 +0500

    express

---
+++ Express/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+app.get('/about',(req,res)=>{
+    res.send('About Us');
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('Contact Us');
+})
+
+app.listen(3000,()=>{});
