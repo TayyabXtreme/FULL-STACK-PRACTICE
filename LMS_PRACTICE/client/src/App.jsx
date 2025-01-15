d4a8ed76bf5d59148079629a7d333f4829fae655
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-15 22:57:58 +0500

    app and login pages

--- LMS_PRACTICE/client/src/App.jsx
+++ LMS_PRACTICE/client/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 import { Button } from './components/ui/button'
 import Login from './pages/Login'
 
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <main>
-     
+     <Navbar/>
     <Login/>
     </main>
   )
