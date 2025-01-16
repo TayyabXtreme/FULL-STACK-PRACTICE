84b5253d2fe50379462fa227ceffde4d4aa3b184
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-16 17:50:17 +0500

    hero section and routing

--- LMS_PRACTICE/client/src/App.jsx
+++ LMS_PRACTICE/client/src/App.jsx
@@ -1,16 +1,38 @@
 
+import { createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import { Button } from './components/ui/button'
 import Login from './pages/Login'
+import HeroSection from './pages/student/HeroSection'
+import MainLayout from './layout/MainLayout'
+import { RouterProvider } from 'react-router'
 
 function App() {
+
+  const appRouter=  createBrowserRouter([
+    {
+      path:'/',
+      element:<MainLayout/>,
+      children:[
+        {
+          path:'/',
+          element:<>
+          <HeroSection/>
+          {/* Courses PAge */}
+          </>
+        },{
+          path:'login',
+          element:<Login/>
+        }
+      ]
+    }
+  ])
   
 
   return (
     <main>
-     <Navbar/>
-    <Login/>
+     <RouterProvider router={appRouter}/>
     </main>
   )
 }
