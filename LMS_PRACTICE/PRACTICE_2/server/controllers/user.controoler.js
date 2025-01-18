233d907520a6448750b64d19f05ec05ee566cb65
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-18 19:13:59 +0500

    logout and updat user logic

--- LMS_PRACTICE/PRACTICE_2/server/controllers/user.controoler.js
+++ LMS_PRACTICE/PRACTICE_2/server/controllers/user.controoler.js
@@ -1,6 +1,7 @@
 import {User} from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 import { generateToken } from '../utils/generateToken.js'
+import { deleteMediaFromCloudinary, uploadMedia } from '../utils/cloudinary.js'
 export const register=async(req,res)=>{
     try {
         const {name,email,password}=req.body
@@ -84,6 +85,103 @@ export const login=async(req,res)=>{
             message:'failed to register'
         })
 
+        
+    }
+}
+
+export const logout=async(_,res)=>{
+    try {
+        return res.status(200).cookie("token","",{maxAge:0}).json({
+            success:true,
+            message:'logout successfully'
+        })
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:'failed to logout'
+        })
+    }
+}
+
+export const getUserProfile=async(req,res)=>{
+    try {
+        const userId=req.id
+        const user=await User.findById(userId).select("-password")
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:'Profile not found'
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            user
+        })
+
+        
+    } catch (error) {
+        console.log(error)
+
+        return res.status(500).json({
+            success:false,
+            message:'failed to get user'
+        })
+    }
+}
+
+export const updateProfile=async(req,res)=>{
+    try {
+        const userId=req.id;
+        const name="tayyab";
+        const profilePhoto=req.file;
+        console.log(profilePhoto)
+        if(!profilePhoto || !name  ){
+            return res.status(400).json({
+                success:false,
+                message:'pls provide user name and photo'
+            })
+        }
+        const user=await User.findById(userId)
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:'Profile not found'
+            })
+
+        }
+        //
+        if(user.photoUrl){
+            const publicId=user.photoUrl.split('/').pop().split('.')[0];//extract public id
+            deleteMediaFromCloudinary(publicId)
+        }
+
+        const cloudResponse=await uploadMedia(profilePhoto.path)
+        const  photoUrl=cloudResponse.secure_url
+
+        const updatedData={name,photoUrl}
+        const updatedUser=await User.findByIdAndUpdate(userId,updatedData,{
+            new: true
+        }).select('-password')
+
+        return res.status(200).json(
+            {
+                success:true,
+                message:'user updated successfully',
+                user:updatedUser
+            }
+
+        )
+        
+    } catch (error) {
+        console.log(error)
+
+        return res.status(500).json({
+            success:false,
+            message:'failed to get user'
+        })
         
     }
 }
