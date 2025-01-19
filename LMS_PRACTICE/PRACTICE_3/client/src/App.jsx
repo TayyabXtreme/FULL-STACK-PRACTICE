20863d0c5e54e82bc1788ab818cad5c1e2b78912
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-19 20:17:41 +0500

    frontend project setup

---
+++ LMS_PRACTICE/PRACTICE_3/client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='bg-red-400' >
+      <Button>HEllo </Button>
+    </div>
+  )
+}
+
+export default App
