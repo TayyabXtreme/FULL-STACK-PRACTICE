c4048c45f909e3860876b151c251a7d225385398
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-23 00:37:46 +0500

    redux query api

--- LMS_PRACTICE/PRACTICE_3/client/src/app/rootReducer.js
+++ LMS_PRACTICE/PRACTICE_3/client/src/app/rootReducer.js
@@ -1,6 +1,8 @@
 
 import { authApi } from '@/features/api/authApi';
 import { courseApi } from '@/features/api/courseApi';
+import { courseProgressApi } from '@/features/api/courseProgressApi';
+import { purchaseApi } from '@/features/api/purchaseApi';
 import authReducer from '@/features/authSlice'
 import { combineReducers } from '@reduxjs/toolkit';
 
@@ -9,6 +11,8 @@ import { combineReducers } from '@reduxjs/toolkit';
 const rootReducer=combineReducers({
     [authApi.reducerPath]:authApi.reducer,
     [courseApi.reducerPath]:courseApi.reducer,
+    [purchaseApi.reducerPath]:purchaseApi.reducer,
+    [courseProgressApi.reducerPath]:courseProgressApi.reducer,
     auth:authReducer
 
 })
