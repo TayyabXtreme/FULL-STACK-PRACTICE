435a6e5053fee51c57017f6d779748f7dda320f5
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-31 22:36:05 +0500

    app and index

---
+++ MERNWITHANEES/forthday/clinet/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import Todo from './components/component/Todo'
+
+const App = () => {
+  
+  return (
+    <Todo/>
+  )
+}
+
+export default App
