a9eec835cc6bd8b9fc0552e0c4d2c10ea7dce53d
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-02-02 23:04:19 +0500

    basicjava

---
+++ MERNWITHANEES/ThirdDay/basicjava.js
@@ -0,0 +1,37 @@
+//array
+//distionay
+//filter
+//map
+
+
+// let array = [1, 3, 6, 34, 33, 6, 3, 34, 54, 77, 99];
+
+// array = array.map((valiable) => {
+//     if (valiable % 2 !== 0) {
+//         return valiable * 2; // Multiply odd numbers by 2
+//     } else {
+//         return valiable; // Keep even numbers unchanged
+//     }
+// });
+
+// console.log("array", array);
+
+
+
+
+// const users = [
+//     { name: "Alice Johnson", email: "alice.johnson@example.com", phone: "123-456-7890" },
+//     { name: "Bob Smith", email: "bob.smith@example.com", phone: "234-567-8901" },
+//     { name: "Charlie Brown", email: "charlie.brown@example.com", phone: "345-678-9012" },
+//     { name: "Diana Prince", email: "diana.prince@example.com", phone: "456-789-0123" },
+//     { name: "Evan Davis", email: "evan.davis@example.com", phone: "567-890-1234" },
+//     { name: "Fiona Clark", email: "fiona.clark@example.com", phone: "678-901-2345" },
+//     { name: "George Miller", email: "george.miller@example.com", phone: "789-012-3456" },
+// ];
+
+// users.forEach((user)=>{
+//     console.log(user.name)
+// })
+
+
+
