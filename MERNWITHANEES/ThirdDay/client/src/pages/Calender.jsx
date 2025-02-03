e73f636af3658594e21d51a0415f46060a0c5564
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-02-03 17:52:58 +0500

    button and calender

---
+++ MERNWITHANEES/ThirdDay/client/src/pages/Calender.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar } from '../components/Navbar'
+
+const Calender = () => {
+  return (
+    
+    <div>
+        <Navbar/>
+        Calender</div>
+  )
+}
+
+export default Calender
