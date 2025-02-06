550de38efb864046001779c50834b8a5f29f4130
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-02-06 22:54:39 +0500

    user router

--- UBER_CLONE_PRACTICE/Backend/routes/user.routes.js
+++ UBER_CLONE_PRACTICE/Backend/routes/user.routes.js
@@ -4,6 +4,7 @@ const express=require('express');
 const router=express.Router();
 const {body}=require('express-validator')
 const userController=require('../controllers/user.controller')
+const authMiddleware=require('../middlewares/auth.middleware')
 
 
 router.post('/register',[
@@ -17,6 +18,9 @@ router.post('/login',[
     body('email').isEmail().withMessage('Please enter a valid email'),
     body('password').isLength({min:6}).withMessage('Password should be atleast 6 characters long')
 ],userController.loginUser)
+
+
+router.get('/profile',authMiddleware.authUser,userController.getUserProfile)
 
 
 
