1b07723ab3dfd2658349d776787a0567b0eba1ad
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-02-09 00:33:04 +0500

    api responsie

---
+++ fullStackApp/mystry-message/src/types/ApiResponse.ts
@@ -0,0 +1,10 @@
+import {Message} from '@/model/User.model'
+
+export interface ApiResponse {
+    success:boolean;
+    message:string;
+    isAcceptingMessage?:boolean
+    messages?:Array<Message>
+
+}
+
