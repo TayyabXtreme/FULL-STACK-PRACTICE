55dde3f9bc6d50c9db893b953a43b4a6b3c0cf04
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-02-14 23:09:31 +0500

    user login

--- Uber-Practice-Clone/Backend/controllers/user.controller.js
+++ Uber-Practice-Clone/Backend/controllers/user.controller.js
@@ -31,3 +31,36 @@ module.exports.registerUser=async(req,res,next)=>{
 
 
 }
+
+module.exports.loginUser=async(req,res,next)=>{
+    try {
+        
+    
+        const errors=validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({error:errors.array()})
+        }
+
+        const {email,password}=req.body
+
+        const user=await userModel.findOne({email}).select('+password')
+
+        if(!user){
+            return res.status(401).json({message:'invalid email or password'})
+
+        }
+
+        const isMatch=await user.comparePassword(password)
+
+        if(!isMatch){
+            return res.status(401).json({message:'invalid email or password'})
+        }
+
+        const token=user.generateAuthToken();
+
+        res.status(200).json({token,user})
+
+    }catch(e){
+        console.log(`user controller error: ${e}`)
+    }
+}
--- Uber-Practice-Clone/Backend/routes/user.routes.js
+++ Uber-Practice-Clone/Backend/routes/user.routes.js
@@ -12,5 +12,13 @@ router.post('/register',[
 userController.registerUser
 )
 
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:6}).withMessage('password must be 6 character ')
+],
+userController.loginUser
+
+)
+
 
 module.exports=router
