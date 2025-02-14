b94352df430867af3426cac25be7c44c0d76d3f0
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-02-14 21:50:11 +0500

    user register

--- Uber-Practice-Clone/Backend/app.js
+++ Uber-Practice-Clone/Backend/app.js
@@ -3,10 +3,15 @@ dotenv.config();
 const express=require('express')
 const cors=require('cors')
 const app=express()
+
+const userRoutes=require('./routes/user.routes')
 const connectToDb=require('./db/dbconnect')
  connectToDb()
 
+ app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.use(cors())
+app.use('/users',userRoutes)
 app.get('/',(req,res)=>{
     res.send('HEllo World')
 });
