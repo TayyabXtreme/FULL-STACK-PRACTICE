ad51efb65ae8630137b69edc6ef3cc947d9ef5d1
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-02-17 00:20:12 +0500

    captain login

---
+++ Uber-Practice-Clone/frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const CaptainLogin = () => {
+     const [input, setInput] = useState({
+            email:'',
+            password:''
+        })
+        const loginHandler=(e)=>{
+            e.preventDefault()
+            console.log(input)
+        }
+  return (
+    <div className='p-7 flex flex-col justify-between h-screen'>
+        <div>
+        <img className='w-20 mb-10' src="https://images.squarespace-cdn.com/content/v1/5f12108f09f6c26e19eab384/1595024071484-A6VUH95AXO829RJY19LK/image-asset.jpeg" alt="" />
+        <form onSubmit={(e)=>loginHandler(e)}>
+        <h3 className='text-lg font-medium mb-2' >What's your email</h3>
+        <input
+            value={input.email}
+            onChange={(e) => setInput((val) => ({ ...val, email: e.target.value }))}
+        className='bg-[#eeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+        type="email" required placeholder='email@example.com' />
+        <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+        <input
+        value={input.password}
+        onChange={(e) => setInput((val) => ({ ...val, password: e.target.value }))}
+        className='bg-[#eeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+        type="password" required placeholder='password' />
+        <button
+        className='bg-[#111] mb-3 text-white font-semibold rounded px-4 py-2  w-full text-lg '
+        >Login</button>
+       <p className='text-center'>Join a fleet?  <Link
+       to={'/captain-signup'}
+        className='text-blue-600'
+        >Register as a Captain</Link></p>
+        </form>
+        </div>
+        <div>
+            <Link
+            to='/login'
+            className='bg-[#d5622d] flex items-center justify-center mb-5 text-white font-semibold rounded px-4 py-2  w-full text-lg ' >Sign in as User</Link>
+        </div>
+    </div>
+  )
+}
+
+export default CaptainLogin
