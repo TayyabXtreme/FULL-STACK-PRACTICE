1bf6fc4f7be6312d879371ba922a0b3ed8e7a39c
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-02-18 19:16:55 +0500

    app

--- Uber-Practice-Clone/frontend/src/App.jsx
+++ Uber-Practice-Clone/frontend/src/App.jsx
@@ -6,21 +6,33 @@ import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
 import { UserDataContext } from './context/UserContext'
+import Start from './pages/Start'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
+import { CaptainDataContext } from './context/CaptainContext'
+import CaptainHome from './pages/CaptainHome'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import CaptainLogut from './pages/CaptainLogut'
+import Ridding from './pages/Ridding'
 
 const App = () => {
   const ans=useContext(UserDataContext)
-  console.log(ans)
+  const cap=useContext(CaptainDataContext)
+  console.log(ans,cap)
   return (
     <>
     <Routes>
 
-      <Route path={'/'} element={<Home/>} />
+      <Route path={'/'} element={<Start/>} />
       <Route path={'/login'} element={<UserLogin/>} />
       <Route path={'/signup'} element={<UserSignup/>} />
       <Route path={'/captain-login'} element={<CaptainLogin/>} />
       <Route path={'/captain-signup'} element={<CaptainSignup/>} />
-
-
+      <Route path='/home' element={<UserProtectWrapper><Home/></UserProtectWrapper>} />
+      <Route path='/user/logout' element={<UserProtectWrapper><UserLogout/></UserProtectWrapper>} />
+      <Route path='/captain/logout' element={<CaptainProtectWrapper><CaptainLogut/></CaptainProtectWrapper>} />
+      <Route path='/captain-home' element={<CaptainProtectWrapper><CaptainHome/></CaptainProtectWrapper>} /> 
+      <Route path='/ridding' element={<Ridding/>} />
     </Routes>
     
     </>
