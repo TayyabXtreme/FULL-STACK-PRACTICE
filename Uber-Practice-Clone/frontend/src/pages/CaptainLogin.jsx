5e30f867332f86e855554bfe9023fc0650339311
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-02-19 11:27:10 +0500

    captain login

--- Uber-Practice-Clone/frontend/src/pages/CaptainLogin.jsx
+++ Uber-Practice-Clone/frontend/src/pages/CaptainLogin.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { CaptainDataContext } from '../context/CaptainContext'
+import axios from 'axios'
 
 const CaptainLogin = () => {
+    const navigate=useNavigate()
+    const {captain,setCaptain}=useContext(CaptainDataContext)
      const [input, setInput] = useState({
             email:'',
             password:''
         })
-        const loginHandler=(e)=>{
+        const loginHandler=async(e)=>{
+    
             e.preventDefault()
+            const response=await axios.post(`${import.meta.env.VITE_BASE_URL}/captains/login`,input)
+            if(response.status==200){
+                const data=response.data
+                setCaptain(data.captain)
+                localStorage.setItem('token',data.token)
+                navigate('/captain-home')
+            }
             console.log(input)
         }
   return (
