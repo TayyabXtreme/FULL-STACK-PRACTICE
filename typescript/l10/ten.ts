9d4308eec49df03248c6e4ab70e906fc1d085b38
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-03-13 23:51:02 +0500

    lec 10-11-12

---
+++ typescript/l10/ten.ts
@@ -0,0 +1,25 @@
+// bacho wala way
+
+class Person1{
+
+    name:string;
+    age:number;
+    constructor(name:string, age:number){
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
+//advaneced way
+
+
+class Person2{
+
+    
+    constructor(public name:string, public age:number){
+        
+    }
+
+
+}
