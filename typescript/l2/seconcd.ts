93c7ad7bf6485521fb3412b0f8cebb8e097fac81
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-03-13 22:02:11 +0500

    lec1 and lec2

---
+++ typescript/l2/seconcd.ts
@@ -0,0 +1,18 @@
+// let count=4
+
+
+// let msg="hello"
+
+
+// enum access{
+//     admin,
+//     user,
+//     reader,
+    
+// }
+
+
+let username:string = "John";
+let userAge:number = 30;
+console.log(username.toUpperCase());
+
